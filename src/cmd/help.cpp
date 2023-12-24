4f4c9321f6a88e3f970b728323bcd53cd9be337f
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2023-12-24 10:31:42 -0500

    merp

--- src/cmd/help.cpp
+++ src/cmd/help.cpp
@@ -1,26 +1,10 @@
 #include "help.h"
 
 namespace Teal {
-
 #ifdef WINDOWS
     void executeHelp(int argc, char *argv[]) {
         auto msg =  "Here is a list of available commands:\n"
         "\tinit:\tInitialize the project.\n"
-        "\tbuild:\tBuilds the project.\n"
-        "\tadd:\tAdds a file to the project.\n" 
-        "\taddc:\tAdds a new class to the project.\n"
-        "\taddl:\tAdds a reference to a library.\n"
-        "\tclean:\tCleans the project.\n";
-
-
-        Logger::log(msg);
-    }
-#endif
-
-#ifdef LINUX
-    void executeHelp(int argc, char *argv[]) {
-        auto msg =  "Here is a list of available commands:\n"
-        "\tinit:\tInitialize the roject.\n"
         "\tbuild:\tBuilds the project.\n"
         "\tadd:\tAdds a file to the project.\n" 
         "\taddc:\tAdds a new class to the project.\n"
