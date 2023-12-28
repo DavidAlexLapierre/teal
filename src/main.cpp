b18a2ed730bb78b201fcf4994e444f00a4aa62c2
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2023-12-28 09:38:53 -0500

    merp:

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "cmdParser.h"
 #include "utils/logger.h"
+#include "cmd/test.h"
 
 int main(int argc, char *argv[]) {
-
+    Teal::test();
     if (argc < 2) {
         Teal::Logger::err("Please add a command. Use parameter \"help\" to get a list of commands.");
 
@@ -13,8 +14,5 @@ int main(int argc, char *argv[]) {
     if (!parser.run(argv[1], argc, argv)) {
         Teal::Logger::err("Failed to run command");
     }
-
-
-
     return 0;
 }
