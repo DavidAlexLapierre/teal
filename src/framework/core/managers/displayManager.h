2b0006ba76fb9a08d68df1c415cdf03de4d04b09
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2024-01-01 19:31:53 -0500

    added documentation

--- src/framework/core/managers/displayManager.h
+++ src/framework/core/managers/displayManager.h
@@ -10,16 +10,29 @@
 namespace FW::Core {
     class DisplayManager {
         public:
+            /// @brief Manager responsible for handling the window
+            /// @param graphicsDevice Graphics device object holding graphics card information
             DisplayManager(std::shared_ptr<Graphics::GraphicDevice> graphicsDevice);
+            /// @brief Destructor for the DisplayManager
             ~DisplayManager();
+            /// @brief Get the reference to the GLFWwindow object
+            /// @return GLFWwindow reference
             GLFWwindow *getWindow() const;
+            /// @brief Terminate GLFW and clear memory
             void terminate();
+            /// @brief Get the viewport dimension of the window
+            /// @return Viewport dimensions of the window
             glm::vec2 getViewport() const;
 
         private:
+            /// @brief Initialize the GLFWwindow
+            /// @return Status code
             int initWindow();
+            /// @brief GLFWwindow reference
             GLFWwindow *window;
+            /// @brief Reference to the graphics device
             std::shared_ptr<Graphics::GraphicDevice> _graphicsDevice;
+            /// @brief Viewport dimensions of the window
             glm::vec2 viewport;
     };
 }
