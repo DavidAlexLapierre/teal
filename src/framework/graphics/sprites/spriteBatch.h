ff7db921d70b1e0a1a2a2ec424676d4267dab3d5
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2024-01-04 11:18:25 -0500

    Adding nits

--- src/framework/graphics/sprites/spriteBatch.h
+++ src/framework/graphics/sprites/spriteBatch.h
@@ -6,6 +6,8 @@
 #include <memory>
 
 namespace Teal {
+    /// @brief Responsible for batching sprites and sending the data
+    /// @brief back to the GPU in order to improve rendering performances
     class SpriteBatch {
         public:
             /// @brief Constructor of the sprite batcher
@@ -13,7 +15,7 @@ namespace Teal {
             SpriteBatch(std::shared_ptr<GraphicsDevice> graphicsDevice);
 
         private:
-            /// @brief Batcher responsible to queue sprites to the gpu
+            /// @brief Batcher responsible to queue sprites to the GPU
             SpriteBatcher _spriteBatcher;
             /// @brief Reference to the graphics device used by the project
             std::shared_ptr<GraphicsDevice> _graphicDevice;
