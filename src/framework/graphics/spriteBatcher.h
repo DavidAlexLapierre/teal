46d60a8396753157694354515c5014ef942f13ba
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2024-01-04 10:59:39 -0500

    adding doc

--- src/framework/graphics/spriteBatcher.h
+++ src/framework/graphics/spriteBatcher.h
@@ -7,9 +7,12 @@
 namespace Teal {
     class SpriteBatcher {
         public:
+            /// @brief Constructor of the sprite batcher
+            /// @param graphicsDevice Reference to the graphics device used by the project
             SpriteBatcher(std::shared_ptr<GraphicsDevice> graphicsDevice);
 
         private:
+            /// @brief Reference to the graphics device used by the project
             std::shared_ptr<GraphicsDevice> _graphicDevice;
     };
 }
