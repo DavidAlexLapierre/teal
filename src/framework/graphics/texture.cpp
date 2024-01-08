b5a275d1f2777dd919b1bcd31bb25ce7bd5c6447
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2024-01-08 12:10:07 -0500

    nit

--- src/framework/graphics/texture.cpp
+++ src/framework/graphics/texture.cpp
@@ -1,12 +1,14 @@
 #include "texture.h"
 
+#include <stb/stb_image.h>
+
 namespace Teal {
     Texture::Texture(std::string imagePath) {
-
+        _data = stbi_load(imagePath.c_str(), 0, 0, 0, 0);
     }
 
     Texture::~Texture() {
-        delete _data;
+        stbi_image_free(_data);
     }
 
     unsigned char Texture::data() const { return *_data; }
